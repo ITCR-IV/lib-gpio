9785d12d590839f07643df3b651f9c884376ae1e
Ignacio Vargas <ignaevc@gmail.com>
2023-10-19 17:23:59 -0600

    Agregar métodos del taller

--- casa_gpio.c
+++ casa_gpio.c
@@ -13,19 +13,15 @@ void close_gpio() {
     gpiod_chip_close(CHIP);
 }
 
-int write_pin(size_t pin_number, int value) {
+int digital_write(size_t pin, int value) {
   if (CHIP == NULL) {
     return -1;
   }
 
-  struct gpiod_line *line = gpiod_chip_get_line(CHIP, pin_number);
+  struct gpiod_line *line = gpiod_chip_get_line(CHIP, pin);
 
   if (line == NULL) {
     return 1;
-  }
-
-  if (gpiod_line_request_output(line, "lib_casa_gpio", value)) {
-    return 2;
   }
 
   if (gpiod_line_set_value(line, value)) {
@@ -37,19 +33,15 @@ int write_pin(size_t pin_number, int value) {
   return 0;
 }
 
-int read_pin(size_t pin_number, int *read_value) {
+int digital_read(size_t pin, int *read_value) {
   if (CHIP == NULL) {
     return -1;
   }
 
-  struct gpiod_line *line = gpiod_chip_get_line(CHIP, pin_number);
+  struct gpiod_line *line = gpiod_chip_get_line(CHIP, pin);
 
   if (line == NULL) {
     return 1;
-  }
-
-  if (gpiod_line_request_input(line, "lib_casa_gpio")) {
-    return 2;
   }
 
   int value = gpiod_line_get_value(line);
@@ -61,6 +53,32 @@ int read_pin(size_t pin_number, int *read_value) {
   *read_value = value;
 
   gpiod_line_release(line);
+
+  return 0;
+}
+
+int pinMode(size_t pin, enum PinMode MODE) {
+  if (CHIP == NULL) {
+    return -1;
+  }
+
+  struct gpiod_line *line = gpiod_chip_get_line(CHIP, pin);
+
+  if (line == NULL) {
+    return 1;
+  }
+
+  switch (MODE) {
+  case INPUT:
+    if (gpiod_line_request_input(line, "lib_casa_gpio")) {
+      return 2;
+    }
+
+  case OUTPUT:
+    if (gpiod_line_request_output(line, "lib_casa_gpio", 0)) {
+      return 2;
+    }
+  }
 
   return 0;
 }
--- casa_gpio.h
+++ casa_gpio.h
@@ -3,13 +3,23 @@
 
 #include <stddef.h>
 
+// Retorna código de salida (0 == OK)
 int init_gpio();
 
 void close_gpio();
 
+enum PinMode { INPUT, OUTPUT };
 
-int write_pin(size_t pin_number, int value);
+// Retorna código de salida (0 == OK)
+int pinMode(size_t pin, enum PinMode MODE);
 
-int read_pin(size_t pin_number, int *read_value);
+// Retorna código de salida (0 == OK)
+int digital_write(size_t pin, int value);
+
+// Retorna código de salida (0 == OK)
+int digital_read(size_t pin, int *read_value);
+
+// freq interpretada en Hz, duration en segundos
+int blink(size_t pin, float freq, float duration);
 
 #endif
