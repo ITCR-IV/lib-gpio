782ae8a6343096807b7f7da2f03fc3d4fc61631d
Ignacio Vargas <ignaevc@gmail.com>
2023-10-19 17:45:10 -0600

    Mover cosas a estructura de directorios

---
+++ src/gpio_prueba.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "casa_gpio.h"
+
+int main() {
+
+  printf("==============================\n");
+  printf("Hola Mundo\n");
+  printf("==============================\n");
+
+  return 0;
+}
