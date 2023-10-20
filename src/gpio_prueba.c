ae66226f4e89c3552c02fa42b2b670dd8a75c12e
Ignacio Vargas <ignaevc@gmail.com>
2023-10-20 13:08:12 -0600

    Printear código de error

--- src/gpio_prueba.c
+++ src/gpio_prueba.c
@@ -6,36 +6,36 @@ int main() {
   int ret;
 
   if ((ret = init_gpio())) {
-    fprintf(stderr, "No se pudo inicializar el GPIO\n");
+    fprintf(stderr, "No se pudo inicializar el GPIO (Error: %d)", ret);
     return ret;
   }
 
   if ((ret = pinMode(20, OUTPUT))) {
-    fprintf(stderr, "No se pudo setear el pin 20 como output\n");
+    fprintf(stderr, "No se pudo setear el pin 20 como output (Error: %d)", ret);
     return ret;
   }
 
   if ((ret = pinMode(21, OUTPUT))) {
-    fprintf(stderr, "No se pudo setear el pin 21 como output\n");
+    fprintf(stderr, "No se pudo setear el pin 21 como output (Error: %d)", ret);
     return ret;
   }
 
   if ((ret = pinMode(16, INPUT))) {
-    fprintf(stderr, "No se pudo setear el pin 16 como input\n");
+    fprintf(stderr, "No se pudo setear el pin 16 como input (Error: %d)", ret);
     return ret;
   }
 
   printf("Seteando el pin 20 en alto (1)\n");
 
   if ((ret = digital_write(20, 1))) {
-    fprintf(stderr, "No se pudo setear el pin 20 alto (1)\n");
+    fprintf(stderr, "No se pudo setear el pin 20 alto (1) (Error: %d)", ret);
     return ret;
   }
 
   printf("Iniciando blink de 0.5Hz por 3 segundos en pin 21\n");
 
   if ((ret = blink(21, 0.5, 5))) {
-    fprintf(stderr, "Falló el blink del pin 21\n");
+    fprintf(stderr, "Falló el blink del pin 21 (Error: %d)", ret);
     return ret;
   }
 
@@ -43,7 +43,7 @@ int main() {
   int valor;
 
   if ((ret = digital_read(16, &valor))) {
-    fprintf(stderr, "Fallo al leer el pin 16\n");
+    fprintf(stderr, "Fallo al leer el pin 16 (Error: %d)", ret);
     return ret;
   }
 
